b77d0463dd3bd55d2103980424d591ce176eca4d
unknown <madhan@piktorlabs.com>
2021-01-08 01:58:52 -0800

    first commit

---
+++ Assignment 2/assets/js/flex.js
@@ -0,0 +1,19 @@
+
+
+
+function grow(x){
+    
+ let a=document.querySelector("#grow");
+ let b=document.querySelector("#columngrow");
+ 
+ if(a) a.id="tiledefault";    
+ if(b) b.id="columndefault";
+     
+    x.id="grow";
+    x.parentNode.id="columngrow";
+
+}
+
+
+
+
